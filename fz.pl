cd69405181cfdcd2c29df09d8e9dc4cb6a135632
leo <leo@LEO-GT60-HOME>
2017-05-06 18:59:27 +0300

    ?

--- fz.pl
+++ fz.pl
@@ -1,3 +1,27 @@
 #!/usr/bin/env -perl
+use Cwd qw(getcwd);
+#print `perl -version`;
+my $cwd = getcwd();
+#print $cwd."\n";		
 
-print "Hello";
+my $dir = $cwd;
+
+&dirrec($dir);
+sub dirrec{
+	my ($curdir) = @_;
+	opendir(my $hendler, $curdir) or die "Can\`t open \"$curdir\".";
+	
+	
+		while(my $curelement = readdir $hendler){
+		next if ($curelement =~ /^\./);
+		my $maynextdir = $curdir."/".$curelement;
+		print $maynextdir."\n" if (-f $maynextdir);
+		if (-d $maynextdir)	
+				{
+					print "Next dir: $maynextdir\n";
+					&dirrec($maynextdir);
+				}
+		}
+}
+
+
